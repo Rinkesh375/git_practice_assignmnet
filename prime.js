806b1ca0b418f778db96cc475d8f7fac4f7e48df
Rinkesh <rinkeshujjwal16@gmail.com>
2022-12-06 18:51:46 +0530

    Prime problem fixed

---
+++ prime.js
@@ -0,0 +1,13 @@
+let factor = 0;
+let num = 21
+for(let i = i;i<=num;i++){
+    if(num%i===0){
+        factor++
+    }
+}
+if(factor===2){
+    console.log("Not Prime")
+}
+else{
+    console.log('Not prime')
+}
